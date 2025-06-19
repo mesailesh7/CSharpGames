84e480f6aebaf167b469d8b9d1e96007a28973ed
Sonu Thapa <37073011+mesailesh7@users.noreply.github.com>
2025-06-19 00:57:37 -0600

    Update Program.cs

--- Guess a Number/Program.cs
+++ Guess a Number/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        int value = Random.Shared.Next(1, 101);
+        while (true)
+        {
+            Console.WriteLine("Guess a number (1-100)");
+            bool valid = int.TryParse((Console.ReadLine() ?? "").Trim(), out int input);
+            if(!valid) Console.WriteLine("Invalid input");
+            else if (input == value) break;
+            else Console.Write($"Incorrect. too {(input < value ? "Low" : "High")}.");
+        }
+        Console.WriteLine("You guessed it");
+        Console.Write("Press any key to exit . . . ");
+        Console.ReadKey(true);
     }
 }

9779b7050d42258abd49d082f6bde66a97534a06
Sonu Thapa <37073011+mesailesh7@users.noreply.github.com>
2025-06-26 17:22:29 -0600

    GuessANumber

---
+++ GuessANumber/Guess a Number/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Guess_a_Number;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int value = Random.Shared.Next(1, 101);
+        while (true)
+        {
+            Console.WriteLine("Guess a number (1-100)");
+            bool valid = int.TryParse((Console.ReadLine() ?? "").Trim(), out int input);
+            if(!valid) Console.WriteLine("Invalid input");
+            else if (input == value) break;
+            else Console.Write($"Incorrect. too {(input < value ? "Low" : "High")}.");
+        }
+        Console.WriteLine("You guessed it");
+        Console.Write("Press any key to exit . . . ");
+        Console.ReadKey(true);
+    }
+}
